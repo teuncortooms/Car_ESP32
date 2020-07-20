98d95df64b6bdb40545dcb71506ce0f2fcbb9708
i884573 <t.cortooms@fontys.nl>
2020-07-20 16:03:49 +0200

    Remove debug lines and add method Stop

--- Car/src/CarController.cpp
+++ Car/src/CarController.cpp
@@ -1,34 +1,30 @@
 #include "CarController.h"
 
-CarController::CarController() : _car(18, 22, 23, 19)
+CarController::CarController(int LeftAcceleratePin, int LeftReversePin, int RightAcceleratePin, int RightReversePin)
+    : _car(LeftAcceleratePin, LeftReversePin, RightAcceleratePin, RightReversePin)
 {
-    _counter = 0;
 }
 
 void CarController::Exec(String message)
 {
     if (message.indexOf("/?GO_UP") > 0 || message == "Up")
     {
-        Serial.print(_counter++);
-        Serial.print(": ");
         _car.GoUp();
     }
     if (message.indexOf("/?GO_DOWN") > 0 || message == "Down")
     {
-        Serial.print(_counter++);
-        Serial.print(": ");
         _car.GoDown();
     }
     if (message.indexOf("/?GO_LEFT") > 0 || message == "Left")
     {
-        Serial.print(_counter++);
-        Serial.print(": ");
         _car.GoLeft();
     }
     if (message.indexOf("/?GO_RIGHT") > 0 || message == "Right")
     {
-        Serial.print(_counter++);
-        Serial.print(": ");
         _car.GoRight();
+    }
+    if (message.indexOf("/?STOP") > 0 || message == "Stop")
+    {
+        _car.Stop();
     }
 }
